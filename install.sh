481a31012de7062e6001ddb2447bc422c26ad0ac
matee <matee@matee.net>
2014-04-22 19:14:36 +0200

    small changes

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-#!zsh
+#!/usr/bin/env bash
 
-ln -s `pwd`/zsh_nocorrect ~/.zsh_nocorrect
-ln -s `pwd`/zprofile ~/.zprofile
-ln -s `pwd`/zshenv ~/.zshenv
-ln -s `pwd`/zshrc ~/.zshrc
+#ln -s `pwd`/zsh_nocorrect ~/.zsh_nocorrect
+#ln -s `pwd`/zprofile ~/.zprofile
+#ln -s `pwd`/zshenv ~/.zshenv
+#ln -s `pwd`/zshrc ~/.zshrc
 
-mkdir -p ~/.pip
-ln -s `pwd`/.pip/pip.conf ~/.pip/
+# Git
+ln -s `pwd`/gitconfig ~/.gitconfig
+ln -s `pwd`/gitignore_global ~/.gitignore_global
+
+# Python
+ln -s `pwd`/pip ~/.pip
+
+# PostgreSQL
+ln -s `pwd`/psqlrc ~/.plsqrc
+# This one should never be linked and committed
+cp `pwd`/.pgpass ~/.pgpass
+
+# TextEditors
+ln -s `pwd`/.nanorc ~/.nanorc
